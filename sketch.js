fb4df9c818ff27d148884430bc221c1bbd04add5
Yukeshwar <123797754+Yukeshwar@users.noreply.github.com>
2024-10-03 00:00:07 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,21 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
+  createCanvas(2000, 2000); 
+  background(255, 204, 0); 
+  let dotSize1 = 30; 
+  let dotSize2 = 60; 
+  let spacing = 100; 
 
+  for (let y = 0; y < h; y += spacing)
+     { 
+    for (let x = 0; x < w; x += spacing)
+      { 
+      fill(0); 
+      noStroke(); 
+      let dotSize = ((x / spacing) + (y / spacing)) % 2 * (dotSize2 - dotSize1) + dotSize1;
+      ellipse(x + spacing / 2, y + spacing / 2, dotSize, dotSize);
+    }
+  }
+}
 function draw() {
+
 }
